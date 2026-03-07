a4c7e82d466e9df6268b5eb69347a27958bbc2e0
kashish12-source <laxmivishnoi33@gmail.com>
2026-03-08 00:04:26 +0530

    Add files via upload
    here we write a code for checking whether the entered user is able to vote or not

---
+++ if_else.py
@@ -0,0 +1,9 @@
+'''the if else statement is used to check the condition'''
+'''lets prepare a program which will identify that if the user is able to vote or not'''
+age=int(input('Enter your age here :'))
+if(age>=18):
+    print('Yes you are able to vote ')
+else:
+    print('You are not abel to give vote')
+
+
